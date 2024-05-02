98173530c62be010aae88da63a25a04904c6770d
charlimora <ca.agudelo.2021@gmail.com>
2024-05-02 13:26:16 -0500

    Ejemplo bases finalizado

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\CursoController;
+use App\Http\Controllers\SaludoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/saludo', [SaludoController::class, 'saludito']);
+
+Route::resource('/cursos', CursoController::class);
+
+
